dd2dd377cf0f25b4518513e49016d83b99ad5007
liuhao0516 <you@example.com>
2020-11-09 17:00:24 +0800

    登录和注册的切换完成

---
+++ assets/js/login.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.login a').on('click', function() {
+        $('.login').hide().next().show()
+    })
+
+    $('.register a').on('click', function() {
+        $('.register').hide().prev().show()
+    })
+
+})
